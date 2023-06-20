e65d583b6d7e1259731470bd4752daf5544627e6
prajhub <pzlmagar@gmail.com>
2023-06-20 17:17:39 +0545

    Added navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import NavBar from "~/components/NavBar";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,6 +11,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <NavBar/>
       <Component {...pageProps} />
     </SessionProvider>
   );
