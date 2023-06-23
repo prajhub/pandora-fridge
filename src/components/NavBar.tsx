bf5a3e653e6787b469784bba4bd1ea98284df147
prajhub <pzlmagar@gmail.com>
2023-06-23 10:00:27 +0545

    Figured out profile menu.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import ProfileMenu from "./ui/ProfileMenu"
 
 export default function NavBar() {
     return (
@@ -10,22 +11,14 @@ export default function NavBar() {
         </Link>
        
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-[#ECECEC] md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-[#ECECEC] dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex items-center flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-[#ECECEC] md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-[#ECECEC] ">
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+              <Link href="/" className="block py-2 pl-3 pr-4 text-white bg-[#5DA2D5] rounded md:bg-transparent md:text-[#5DA2D5] md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
             </li>
             <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
+              <ProfileMenu/>
             </li>
-            <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-            </li>
+          
           </ul>
         </div>
       </div>
---
+++ src/components/ui/ProfileMenu.tsx
@@ -0,0 +1,59 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
+import {Menu, Transition} from '@headlessui/react'
+import {Fragment} from 'react'
+
+
+
+export default function ProfileMenu() {
+
+    const {data: sessionData} = useSession()
+
+    const userImage = sessionData?.user?.image
+
+
+    return (
+        <>
+        
+      { sessionData ? <div className="relative inline-block text-left">
+        <Menu>
+          {({ open }) => (
+            <>
+              <Menu.Button className="inline-flex items-center px-4 py-2 text-sm font-medium text-white  focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                {userImage && typeof userImage === 'string' && (
+                  <img src={userImage} alt="User Profile" className="w-10 h-10 rounded-full" />
+                )}
+              
+              </Menu.Button>
+  
+              <Transition
+                show={open}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items
+                  static
+                  className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
+                  <div className="px-4 py-3">
+                    <p className="mb-2 text-sm">{sessionData?.user?.name}</p>
+                    <button
+                      className="flex items-center justify-center w-full px-4 py-2 text-sm text-red-500 hover:bg-red-100"
+                      onClick={sessionData ? () => void signOut() : () => void signIn()}
+                    >
+                        {sessionData ? "Sign out" : "Sign in"}
+                    </button>
+                  </div>
+                </Menu.Items>
+              </Transition>
+            </>
+          )}
+        </Menu>
+      </div> : <button type="button" onClick={sessionData ? () => void signOut() : () => void signIn()} className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-20">Sign In</button>}
+      </>
+    )
+}
